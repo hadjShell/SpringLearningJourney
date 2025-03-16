05e1cc6a0341f2417ddb2efe84c3b52747a9a64f
张家源 <davidzhang0880@gmail.com>
2025-03-16 19:39:06 +0000

    Setup JobAd project

---
+++ projects/JobAd/src/main/java/com/hadjshell/JobAd/JobAdApplication.java
@@ -0,0 +1,13 @@
+package com.hadjshell.JobAd;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JobAdApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JobAdApplication.class, args);
+	}
+
+}
---
+++ projects/JobAd/src/test/java/com/hadjshell/JobAd/JobAdApplicationTests.java
@@ -0,0 +1,13 @@
+package com.hadjshell.JobAd;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JobAdApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
